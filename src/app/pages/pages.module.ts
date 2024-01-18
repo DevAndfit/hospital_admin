5c941f64f62d8dd5597051c4abef2bc5491ce959
Francisco <fco.t.quintero@hotmail.com>
2024-01-19 00:16:12 +0100

    fin de la seccion 7

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,23 +10,31 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { ProgressComponent } from './progress/progress.component';
 import { PagesComponent } from './pages.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
 
 @NgModule({
-  declarations: [
-	DashboardComponent,
-	Grafica1Component,
-	ProgressComponent,
-	PagesComponent
-  ],
-  imports: [
-    CommonModule,
-    PagesRoutingModule,
-	 SharedModule,
-	 FormsModule,
-	 ComponentsModule,
-
-  ]
+	declarations: [
+		DashboardComponent,
+		Grafica1Component,
+		ProgressComponent,
+		PagesComponent,
+		AccountSettingsComponent
+	],
+	imports: [
+		CommonModule,
+		PagesRoutingModule,
+		SharedModule,
+		FormsModule,
+		ComponentsModule,
+	],
+	exports: [
+		DashboardComponent,
+		Grafica1Component,
+		ProgressComponent,
+		PagesComponent,
+		AccountSettingsComponent
+	]
 })
 export class PagesModule { }
